eff0a79c9e06836d7494eabe3e468174c9db1903
jesusmgomper <jesusmgomper@gmail.com>
2025-12-05 09:39:17 +0100

    suma

--- pruebaJava/src/pruebaJava/pruebaJava.java
+++ pruebaJava/src/pruebaJava/pruebaJava.java
@@ -4,6 +4,7 @@ public class pruebaJava {
 public static void main(String[] args) {
 	System.out.println("Segunda prueba");
 	System.out.println("----------------");
-
+	int suma = 2 + 2;
+	System.out.println("Suma:" + suma);
 }
 }
