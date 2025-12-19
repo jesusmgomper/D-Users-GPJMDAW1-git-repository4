1a5e82cf1a2bc68d0cfe3a3b839235a18b7b9c41
jesusmgomper <jesusmgomper@gmail.com>
2025-12-19 09:48:01 +0100

    Nuevo fichero objeto de la clase Persona

---
+++ pruebaJava/src/pruebaJava/persona.java
@@ -0,0 +1,17 @@
+package pruebaJava;
+
+public class persona {
+private String nombre;
+private String apellidos;
+private String fechaNacimiento;
+
+public persona(String nombre, String apellidos, String fechaNacimiento) {
+	super();
+	this.nombre = nombre;
+	this.apellidos = apellidos;
+	this.fechaNacimiento = fechaNacimiento;
+}
+
+
+
+}
